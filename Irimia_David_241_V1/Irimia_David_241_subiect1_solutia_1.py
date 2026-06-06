c16d45b7b1aba548a125a0c89f0679e041fd64a6
INDavid04 <davidirimia100@gmail.com>
2026-06-06 12:10:55 +0300

    add: colocviu 6 iunie 2026

---
+++ Irimia_David_241_V1/Irimia_David_241_subiect1_solutia_1.py
@@ -0,0 +1,9 @@
+# Punct din oficiu
+
+#################
+# Organizatoric #
+#################
+
+# IA (Inteligenta artificiala) | Colocviu 6 Iunie 2026 | Varianta 1 
+# Nume: Irimia David
+# Grupa: 241
